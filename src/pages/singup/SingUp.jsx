88bcf8ef0ec92c8cadd700048ea347402a4a5158
sina jahan <sinajahan418@example.com>
2024-05-10 03:24:54 -0700

    round one from claint

--- src/pages/singup/SingUp.jsx
+++ src/pages/singup/SingUp.jsx
@@ -1,56 +1,105 @@
-import React, { useState } from 'react';
-import { FaRegUserCircle } from "react-icons/fa";
-import {Link} from "react-router-dom"
+import React, { useState } from "react";
+// import { FaRegUserCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
-
+import loginIcon from "../../assest/signin.gif";
+import imageTobase64 from "../../hellpers/ImgTobeas64";
 
 const SingUp = () => {
   const [shoPass, setShoPass] = useState(false);
+  const [profile, setProfile] = useState("");
 
+  const handleFile = async(e)=> {
+     const file=  setProfile(e.target.files[0])
+    const image = await imageTobase64(file)
+    console.log(image);
+    setProfile((preve)=>{
+        return{
+            ...preve,
+            profile: image
+        }
+
+    })
+  }
   return (
     <div className=" w-full ">
-    <div className="flex-col flex justify-center items-center mt-[10rem]">
-      <p><FaRegUserCircle className=" size-7 cursor-pointer" /></p>
-      <h1>welcome</h1>
-      <form className=" mt-5">
-        <div className=" mt-4 flex flex-col">
-          <label>
-            <span>email</span>
+      <div className="flex-col flex justify-center items-center mt-[10rem]">
+        <div className=" flex-col gap-6 flex justify-center items-center text-[14px]">
+          {" "}
+          <img
+            className=" flex justify-center items-center mr-5 text-center size-9 cursor-pointer"
+            src={profile  || loginIcon}
+            alt=""
+          />{" "}
+          <p  className="ml-[-17px] mt-10 border-zinc-700 border-[1px] absolute rounded-3xl ">
+            chose file
+          </p>
+          <label className=" opacity-0 relative">
+            <input
+              files={profile}
+              onChange={handleFile}
+              className=" outline-none border-0"
+              type="file"
+            />
           </label>
-          <input className=" bg-slate-300  rounded-2xl" type="email" placeholder="email"/>
         </div>
-        <div className=" mt-4 flex flex-col">
-          <label>
-            <span>name</span>
-          </label>
-          <input className=" bg-slate-300  rounded-2xl" type="text" placeholder="password"/>
-        </div>
-        <div className=" mt-4 flex flex-col">
-          <label>
-            <span>password</span>
-          </label>
-          <input className=" bg-slate-300  rounded-2xl" type={shoPass ? "text" : "password"} placeholder="password"/>
-          {shoPass ? (
+        <form className=" mt-5">
+          <div className=" mt-4 flex flex-col">
+            <label>
+              <span>email</span>
+            </label>
+            <input
+              className=" bg-slate-300  rounded-2xl"
+              type="email"
+              placeholder="email"
+            />
+          </div>
+          <div className=" mt-4 flex flex-col">
+            <label>
+              <span>name</span>
+            </label>
+            <input
+              className=" bg-slate-300  rounded-2xl"
+              type="text"
+              placeholder="password"
+            />
+          </div>
+          <div className=" mt-4 flex flex-col">
+            <label>
+              <span>password</span>
+            </label>
+            <input
+              className=" bg-slate-300  rounded-2xl"
+              type={shoPass ? "text" : "password"}
+              placeholder="password"
+            />
+            {shoPass ? (
               <FaEyeSlash onClick={() => setShoPass(false)} />
             ) : (
               <FaEye onClick={() => setShoPass(true)} />
             )}
+          </div>
+          <div className=" mt-4 flex flex-col">
+            <label>
+              <span>confirm password</span>
+            </label>
+            <input
+              className=" bg-slate-300  rounded-2xl"
+              type="password"
+              placeholder="password"
+            />
+          </div>
+        </form>
+        <div>
+          <button className="p-3 mt-6 bg-slate-400 rounded-lg hover:text-zinc-700">
+            SingUp
+          </button>
         </div>
-        <div className=" mt-4 flex flex-col">
-          <label>
-            <span>confirm password</span>
-          </label>
-          <input className=" bg-slate-300  rounded-2xl" type="password" placeholder="password"/>
-        </div>
-      </form>
-      <div>
-          <button className="p-3 mt-6 bg-slate-400 rounded-lg hover:text-zinc-700">SingUp</button>
+        <Link to="/login">already have accont</Link>
       </div>
-      <Link to="/login">already have accont</Link>
     </div>
-  </div>
   );
-}
+};
 
 export default SingUp;
